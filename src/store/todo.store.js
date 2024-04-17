ce578ad4b408a19b74c333d8ec23b873d34c9726
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-04-16 19:22:41 -0600

    Adding fuctions to store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -21,6 +21,50 @@ const initStore = () => {
     console.log('InitStore')
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented')
+
+}
+
+
+const getTodos = () => {
+    throw new Error('Not implemented')
+
+}
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not implemented')
+}
+
+const toggleTodo = (id) => {
+    throw new Error('Not implemented')
+}
+const deleteTodo = (id) => {
+    throw new Error('Not implemented')
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented')
+}
+const setFilter = (filter = Filters.All) => {
+    throw new Error('Not implemented')
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented')
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
+
 }
